7385a5b557caa7db5c49261b5fdffa1ee45604a2
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-01-07 21:15:53 +0100

    Create Newspaper.java

---
+++ src/model/Newspaper.java
@@ -0,0 +1,72 @@
+package model;
+
+public class Newspaper 
+{
+	private int id;
+	private String name;
+	private String date;
+	private String title;
+	private String author;
+	private String content;
+	
+	public int getId() 
+	{
+		return id;
+	}
+	
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	
+	public String getName() 
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getDate() 
+	{
+		return date;
+	}
+	
+	public void setDate(String date)
+	{
+		this.date = date;
+	}
+	
+	public String getTitle()
+	{
+		return title;
+	}
+	
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+	
+	public String getAuthor()
+	{
+		return author;
+	}
+	
+	public void setAuthor(String author)
+	{
+		this.author = author;
+	}
+	
+	public String getContent()
+	{
+		return content;
+	}
+	
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
+	
+}
