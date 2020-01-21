d52977665126fd0384625bd1ecb28b77a8f2be67
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-01-21 23:59:58 +0100

    add classes of results

---
+++ src/model/Author_dim_Result1.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Author_dim_Result1 
+{
+	private String val;
+	private int nb;
+	
+	public Author_dim_Result1(String val, int nb)
+	{
+		super();
+		this.val = val;
+		this.nb = nb;
+	}
+	
+	public Author_dim_Result1()
+	{
+		super();
+	}
+
+	public String getVal() 
+	{
+		return this.val;
+	}
+
+	public void setVal(String val)
+	{
+		this.val = val;
+	}
+
+	public int getNb() 
+	{
+		return this.nb;
+	}
+
+	public void setNb(int nb)
+	{
+		this.nb = nb;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Author_dim_Result1 [val=" + val + ", nb=" + nb + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/model/Date_dim_Result1.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Date_dim_Result1 
+{
+	private String val;
+	private int nb;
+	
+	public Date_dim_Result1(String val, int nb)
+	{
+		super();
+		this.val = val;
+		this.nb = nb;
+	}
+	
+	public Date_dim_Result1()
+	{
+		super();
+	}
+
+	public String getVal() 
+	{
+		return this.val;
+	}
+
+	public void setVal(String val) 
+	{
+		this.val = val;
+	}
+
+	public int getNb() 
+	{
+		return this.nb;
+	}
+
+	public void setNb(int nb)
+	{
+		this.nb = nb;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Date_dim_Result1 [val=" + val + ", nb=" + nb + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
